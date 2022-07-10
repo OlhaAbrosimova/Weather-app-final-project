60e57af88ee6eedb53032fb31a634e286db4f96d
Olha Abrosimova <abrosimova2020@ukr.net>
2022-07-10 14:33:38 +0300

    update time without timestamp

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-function formatDate(timestamp) {
-	let date = new Date(timestamp);
+// function formatDate(timestamp) {
+// 	let date = new Date(timestamp);
+// 	let hours = date.getHours();
+// 	if (hours < 10) {
+// 		hours = `0${hours}`;
+// 	}
+// 	let minutes = date.getMinutes();
+// 	if (minutes < 10) {
+// 		minutes = `0${minutes}`;
+// 	}
+
+// 	let days = [
+// 		"Sunday",
+// 		"Monday",
+// 		"Tuesday",
+// 		"Wednesday",
+// 		"Thursday",
+// 		"Friday",
+// 		"Saturday",
+// 	];
+// 	let day = days[date.getDay()];
+
+// 	return `${day}, ${hours}:${minutes}`;
+// }
+
+function formatDate() {
+	let date = new Date();
 	let hours = date.getHours();
 	if (hours < 10) {
 		hours = `0${hours}`;
@@ -29,8 +54,8 @@ function formatDay(timestamp) {
 	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 	return days[day];
 }
+
 function displayForecast(response) {
-	
 	let forecast = response.data.daily;
 	let forecastElement = document.querySelector("#forecast");
 
@@ -87,7 +112,7 @@ function displayTemperature(response) {
 	let windElement = document.querySelector("#wind-speed");
 	windElement.innerHTML = response.data.wind.speed.toFixed(1);
 	let dateElement = document.querySelector("#date");
-	dateElement.innerHTML = formatDate(response.data.dt * 1000);
+	dateElement.innerHTML = formatDate();
 	let iconElement = document.querySelector("#icon");
 	iconElement.setAttribute(
 		"src",
